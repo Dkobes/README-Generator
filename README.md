2a27f4040ef0bedf93a7c7182317bf434f7fc053
Danyelle <dkobes15@gmail.com>
2025-01-20 15:27:25 -0600

    Final updates

---
+++ README.md
@@ -0,0 +1,18 @@
+# README Generator
+
+  ## Description
+  Allows creators to make a professional README for their projects, to get to their project quicker
+
+  ## Installation
+  npm install inquirer
+
+  ## Usage
+  No
+
+  ## Credits
+  When running into errors, copilot was used to try to determine the error
+
+  ## License
+  No
+  
+  
--- index.js
+++ index.js
@@ -1,45 +1,65 @@
 // TODO: Include packages needed for this application
 import Inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [ 
     {
         type: 'input',
-        name: 'Title',
+        name: 'title',
         message: 'What is the name of your project?'
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'Provide a description of what your project does',
     },
     {
         type: 'input',
-        name: 'Installation',
+        name: 'installation',
         message: 'Does anything need to be installed? If so, please list.',
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'Are there any instructions that should be followed?',
     },
     {
         type: 'input',
-        name: 'Credits',
+        name: 'credits',
         message: 'Did anyone work with you? Did you have any third-party help? If so please list their information.',
     },
     {
         type: 'input',
-        name: 'License',
+        name: 'license',
         message: 'Does this need a specific license?'
-    }
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeReadMe(data) {
+const content = generateMarkdown(data);
+
+    fs.writeFile('README.md', content, (err) => {
+        if (err) {
+            console.error('Error writing the README file:', err);
+        } else {
+            console.log('README file has been created successfully!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    Inquirer.prompt(questions)
+    .then((answers) => {
+        writeReadMe(answers);
+    })
+    .catch((error) => {
+        console.error('Error initializing:', error);
+    })
+}
 
 // Function call to initialize app
 init();
